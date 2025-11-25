1755821088b4c50240e47569eddee08ef8c83ff6
Roland-Scholz <rolandscholz@gmx.de>
2025-11-25 18:56:39 +0100

    revert to 14.10.2025

---
+++ build/percom.cmd
@@ -0,0 +1,51 @@
+@echo off
+
+call var-def.cmd
+set REL=%REL%\atari
+set SRC=%SRC%\utils
+
+call :compile_module percom 2000
+if NOT %result%==0 goto ende
+call :compile_module percom_run 2e0
+if NOT %result%==0 goto ende
+
+
+cd %REL%
+copy ..\%RES%\mydos90k.atr .
+
+copy /Y /B percom.com + percom_run.com autorun.ar0
+
+..\%TOOLS%\xfddos -i mydos90k.atr autorun.ar0
+..\%TOOLS%\xfddos -i mydos90k.atr ..\firmware\Sally2\sally2.com
+
+mkdir obj 2> nul > nul
+mkdir lst 2> nul > nul
+
+move *.lst lst 2> nul > nul
+move *.o obj 2> nul > nul
+move *.a obj 2> nul > nul
+
+rem copy boot.atr e:
+"C:\Program Files (x86)\Altirra\Altirra64.exe" mydos90k.atr
+rem aspeqt mydos90k.atr
+rem pause
+goto eof
+
+:ende
+
+pause
+goto eof
+
+
+:compile_module
+%CC65%\ca65 -l  %REL%\%1.lst %SRC%\%1.a65 -I %INC% -I %COMMON%\inc -o %REL%\%1.o
+set result=%ERRORLEVEL%
+
+if %result%==0 (
+
+	%CC65%\ld65 -t none %REL%\%1.o -o %REL%\%1.a
+	%CC65%\bin2hex %REL%\%1.a %REL%\%1.hex -o %2
+	java -jar %TOOLS%\Obj2Com\jar\ObjUtil.jar Obj2Com %REL%\%1.a %2
+)
+
+:eof
--- src/version.asm
+++ src/version.asm
@@ -1,1 +1,1 @@
-SVERSION DB "sally-1.0.4-6-gb" 
+SVERSION DB "sally-1.0.4-19-g" 
